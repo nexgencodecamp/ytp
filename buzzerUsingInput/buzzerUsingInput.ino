c447c9567ab3b8ec3b9ffc4940c1f518d4c2c99d
Connellkai <connellkai@gmail.com>
2018-03-12 14:43:52 +1100

    Add files via upload

---
+++ buzzerUsingInput/buzzerUsingInput.ino
@@ -0,0 +1,27 @@
+
+
+
+const int buttonPin = 2;     
+const int buzzerPin =  3;     
+
+int buttonState = 0;   
+
+void setup() {
+  //initialise the pins you will be using
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // buzzer makes noise
+    digitalWrite(buzzerPin, HIGH);
+  } else {
+    // buzzer doesnt make noise
+    digitalWrite(buzzerPin, LOW);
+  }
+}
