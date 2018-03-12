3858973c464f37b30dad52cd0e60ff7717f33274
Connellkai <connellkai@gmail.com>
2018-03-12 16:58:47 +1100

    Add files via upload

---
+++ arduino/sketches/buzzer/buzzer.ino
@@ -0,0 +1,13 @@
+
+const int buttonPin = 2; 
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  pinMode(buttonPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(buttonPin, HIGH);   // since the circuit is not complete till the button is pressed down this can always be on high
+}
