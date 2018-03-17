7c433874aed04778e34ae61f8f0dcbdd0101fcac
Peter Januarius <pete@nexgencodecamp.com.au>
2018-03-18 10:57:12 +1100

    Update instructions to button-test-extension.ino

--- arduino/sketches/button-test-extension/button-test-extension.ino
+++ arduino/sketches/button-test-extension/button-test-extension.ino
@@ -1,8 +1,33 @@
+/*******************************************************************************************************************************
+ 
+      Week 7 - Extension Activity - Connect an LED that turns on when you press the button
+      
+      WHAT TO DO:
+      Build the CIRCUIT first then the CODE. 
+      Once complete, verify your code by compiling it. If it compiles, upload it to your Arduino and test it.
+      
+      HOW TO DO IT:
+      
+      Build the Circuit
+      1. Connect a jumper from pin 12 to anywhere on the breadboard
+      2. Connect an LED to the jumper
+      3. Connect a resistor to the LED
+      4. Connect a jumper from the resistor to the ground (GND) rail
+      
+      Write the Code
+      1. Open the sketch ‘button-test-extension’
+      2. Create a variable called 'LED' and set it to pin 12
+      3. Make 'LED' an output pin in the setup() function
+      4. In the loop() function there is an 'if' statement.
+         In the 'if' statement, if buttonValue is LOW then write a value of LOW to LED, otherwise write a value of HIGH to LED
+      
+ ********************************************************************************************************************************/
+
 // Create a variable called button and set it to 2 which means pin 2 on the Arduino
 int button = 2;
 
 // Create a variable called 'LED' and set it to pin 12
-int LED = 12;
+
 
 void setup() {
   // Setup the serial monitor
@@ -11,8 +36,8 @@ void setup() {
   // Make 'button' an input pin
   pinMode(button, INPUT);
 
-  // Make 'LED' and output pin
-  pinMode(LED, OUTPUT);
+  // Make 'LED' an output pin
+
 }
 
 void loop() {
@@ -25,11 +50,11 @@ void loop() {
   
   if(buttonValue == LOW){
     // Write a value of LOW to the LED pin
-    digitalWrite(LED, LOW);
+
   }
   else{
     // Write a value of HIGH to the LED pin
-    digitalWrite(LED, HIGH);
+  
   }
 
   // Write the value of button to the serial monitor
