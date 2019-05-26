9bb8820973ad1e7fb0b6b08ff9f7a122a6f8dc27
Peter Januarius <pete@nexgencodecamp.com.au>
2019-05-26 21:39:49 +1000

    Added ultrasonic sensor

---
+++ modules/arduino/07-ultrasonics/code/ultrasonic-light-exercise/ultrasonic-light-exercise.ino
@@ -0,0 +1,66 @@
+/*
+* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+*
+* Crated by Dejan Nedelkovski,
+* www.HowToMechatronics.com
+*
+*/
+// defines pins numbers
+const int outPin = 2;
+const int trigPin = 4;
+const int echoPin = 3;
+
+void setup() {
+  pinMode(outPin, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // Get the distance currently measured by the Ultrasonic sensor
+  int distance = getDistance();
+  
+  if(distance > 300){
+    /*
+      On line 28, write a HIGH value to pin number 2 which is a digital pin
+    */
+    
+  }
+  else{
+    /*
+      On line 34, write a LOW value to pin number 2 which is a digital pin
+    */
+    
+  }
+}
+
+
+/*
+ * DO NOT WRITE ANYTHING BELOW THIS LINE
+ */
+
+int getDistance() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  long duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  int distance = duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  // Return the distance
+  return distance;
+}
+
